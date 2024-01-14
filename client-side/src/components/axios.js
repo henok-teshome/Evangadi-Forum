69c4af8e7db61e97ffff91694130cb21a5bbfbea
Henok <henok_206@yahoo.com>
2024-01-14 10:33:00 +0300

    update axios

--- client-side/src/components/axios.js
+++ client-side/src/components/axios.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const axiosBase = () => {
     return axios.create({
-        baseURL: "https://evangadi-forumbackend2024.onrender.com/api",
+        // baseURL: "https://evangadi-forumbackend2024.onrender.com/api",
         // baseURL: "http://localhost:7000/api",
+        baseURL: "https://evangadi-forume.onrender.com",
     });
 };
 export default axiosBase;
--- client-side/src/components/axiosConfig.js
+++ client-side/src/components/axiosConfig.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosBase = axios.create({
     //baseURL: "http://localhost:7000/api/"
-    baseURL: "https://evangadi-forumbackend2024.onrender.com/api",
+    //baseURL: "https://evangadi-forumbackend2024.onrender.com/api",
+    baseURL: "https://evangadi-forume.onrender.com",
 });
 export default axiosBase;
